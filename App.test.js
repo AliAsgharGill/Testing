8ae2b0ab7869da5fdf6fac4feb1cba3d794ddb02
AliAsgharGill <stylinalivlogs@gmail.com>
2024-06-05 19:56:07 +0500

    minor

--- App.test.js
+++ App.test.js
@@ -1,7 +1,9 @@
+/* This code snippet is a set of unit tests written in JavaScript using the Jest testing framework and
+the React Testing Library. Here's a breakdown of what the code is doing: */
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./src/App";
-import "@testing-library/jest-dom"; // Optional, for additional matchers
+import "@testing-library/jest-dom";
 
 test("Test First React App Case", () => {
   render(<App />);
@@ -11,10 +13,70 @@ test("Test First React App Case", () => {
   expect(imageTitle).toBeInTheDocument();
 });
 
+test("Image onChange", () => {
+  render(<App />);
+  const image = screen.getByRole("textbox");
+  fireEvent.change(image, { target: { value: "Lemon" } });
+  expect(image.value).toBe("Lemon");
+});
+
+test("onChange should be called", () => {
+  render(<App />);
+  const input = screen.getByRole("textbox");
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveValue("");
+  const onChange = jest.fn();
+  input.addEventListener("change", onChange);
+  expect(onChange).toHaveBeenCalledTimes(0);
+});
+
 test("should have input element", () => {
   render(<App />);
+
   const inputExists = screen.getByRole("textbox");
   expect(inputExists).toBeInTheDocument();
-  const input = screen.getByPlaceholderText("Lemon");
-  expect(input).toBeInTheDocument();
+  expect(inputExists).toHaveAttribute("name", "Lemon");
+  expect(inputExists).toHaveAttribute("id", "Lemon");
+  expect(inputExists).toHaveAttribute("type", "text");
+
+  const inputPlaceholder = screen.getByPlaceholderText("Lemon");
+  expect(inputPlaceholder).toBeInTheDocument();
+
+  const inputTitle = screen.getByTitle("Lemon");
+  expect(inputTitle).toBeInTheDocument();
+});
+
+test("onClick of Button", () => {
+  render(<App />);
+  const button = screen.getByRole("button");
+  expect(button).toBeInTheDocument();
+  fireEvent.click(button);
+  // for checking when button get clicked, is it showing vite on the screen
+  expect(screen.getByText("Vite")).toBeInTheDocument();
+  // for checking when button get clicked, is it changing button text
+  expect(button.textContent).toBe("Vite");
+  // expect(screen.getByText("Vite")).toBeInTheDocument();
+});
+
+// we can use describe to group tests into categories also we can use test.skip to skip a test and test.only to only run a test
+describe("Image Testing", () => {
+  test("should render an image", () => {
+    render(<App />);
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+  });
+
+  test("image with title", () => {
+    render(<App />);
+    const imageTitle = screen.getByTitle("Lemon In Water");
+    expect(imageTitle).toBeInTheDocument();
+  });
+});
+
+describe.skip("Image Testing With Alt", () => {
+  test("should render an image with title", () => {
+    render(<App />);
+    const imageAlt = screen.getByAltText("Lemon In Water");
+    expect(imageAlt).toBeInTheDocument();
+  });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-
+  const [data, setData] = useState("React")
   return (
     <>
-      <p>First React Test case</p>
+      <p>First React Test case using {data} </p>
+      {/* <button onClick={() => data === "Vite" ? setData("React") : setData("Vite")} >Change</button> */}
+      <button onClick={() => setData("Vite")} >{data}</button>
       <div>
-        <input type="text" placeholder='Lemon' />
+        <input type="text" placeholder='Lemon' title='Lemon' name='Lemon' id='Lemon' className='Lemon' />
       </div>
       <br />
-      <img src="https://images.unsplash.com/photo-1459802071246-377c0346da93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3BsYXNofGVufDB8fDB8fHww" alt="" title='Lemon In Water' />
+      <img src="https://images.unsplash.com/photo-1459802071246-377c0346da93?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c3BsYXNofGVufDB8fDB8fHww" alt="Lemon In Water" title='Lemon In Water' />
+
+
     </>
   )
 }
